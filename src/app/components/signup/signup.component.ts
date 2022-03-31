79067f30a46549f5cb774dff65f62c2fde6796ad
mustafaislek <islekmustafaa@gmail.com>
2022-04-01 01:16:35 +0300

    Add signin-signup components

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signupForm!: FormGroup;
+
+  constructor(
+   public fb: FormBuilder,
+   public router: Router,
+   public authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.creteSignUpForm();
+
+  }
+
+  creteSignUpForm(): void {
+    this.signupForm = this.fb.group({
+      name: [null, Validators.required],
+      surname: [null, Validators.required],
+      age: 15,
+      email: [null, Validators.required],
+      password: [null, Validators.required],
+      token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+    });
+  }
+
+  registerUser() {
+    this.authService.signUp(this.signupForm.value).subscribe((res: any) => {
+      if (res) {
+        this.signupForm.reset();
+        this.router.navigate(['log-in']);
+      }
+    });
   }
 
 }
