08f3939a7ce0b1a3c1494ef0ff6183aa579cfb17
mustafa.islek <mustafa.islek@derinarge.com>
2022-04-04 09:36:24 +0300

    Add delete dialog and row editing buttons

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -4,6 +4,10 @@ import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
 import { Product } from 'src/app/models/product';
 import { ProductsDataSource } from './products-datasource';
+import {
+  ProductDeleteDialogComponent
+} from "../product-delete-dialog/product-delete-dialog.component";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-products',
@@ -17,9 +21,11 @@ export class ProductsComponent implements AfterViewInit {
   dataSource: ProductsDataSource;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['title', 'description', 'price'];
+  displayedColumns = ['title', 'description', 'price', 'view', 'edit', 'delete',];
 
-  constructor() {
+  constructor(
+    public dialog: MatDialog
+  ) {
     this.dataSource = new ProductsDataSource();
   }
 
@@ -28,4 +34,16 @@ export class ProductsComponent implements AfterViewInit {
     this.dataSource.paginator = this.paginator;
     this.table.dataSource = this.dataSource;
   }
+
+  openDeleteDialog(product: Product): void {
+    const dialogRef = this.dialog.open(ProductDeleteDialogComponent);
+    dialogRef.componentInstance.product = product;
+
+    dialogRef.afterClosed().subscribe((result: any) => {
+      if (result.event === 'delete') {
+        // this.loadPage();
+      }
+    });
+  }
+
 }
